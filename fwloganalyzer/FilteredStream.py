ab24007dfc5aa7db77a0da644ae45290821d854e
Zéé Háá <haazee@ymail.com>
2014-06-30 09:01:30 +0200

    A FilteredStream osztály konstruktorában lecseréltem a korábbi string paramétert egy re.compile által visszaadott objektumra. Így talán rugalmasabban használható a későbbiekben. Hátránya, hogy az osztály használatához importálni kell a re modult, amennyiben a keresőkifejezést meg akarom változtatni. Előnye, hogy ez legalább már OO-nak látszik. :) Eredetileg azért írtam át, mert nem tetszett, hogy az így megtalált sorokat a filterhez hasonló, de attól kissé eltérő regexp segítségével kell szétrobbantani mezőkre. (openwrt-s syslog-ng által írt logban kereséshez elég pl. a "^.*\.kernel: (ACCEPT|DROP)" kifejezés, de a szétszedéskor figyelembe kell venni a sor elején megjelenő dátum-idő részeket is, ami valószínűleg gyakrabban változik, mint a fenti kifejezés.

--- fwloganalyzer/FilteredStream.py
+++ fwloganalyzer/FilteredStream.py
@@ -17,21 +17,20 @@ class FilteredStream(object):
         readableObject = egy objektum, ami rendelkezik readline metódussal. A readline-tól elvárt működés,
             visszaad egy stringet (illetve bármely olyan objektumot, amiben re modul - regexp - segítségével keresni lehet),
             None-t, ha elfogytak az adatok. (tehát e tekintetben úgy műjödik, mintha egy fájlból olvasnék)
-        filterExpression = regexp, amit a re modul keresőkifejezésként képes használni
+        filterObject = re.compile által visszaadott objektum
         
     '''
-    def __init__(self, readableObject, filterExpression=r".*kernel: (DROP|ACCEPT) "):
+    def __init__(self, readableObject, filterObject=re.compile(r".*kernel: (DROP|ACCEPT) ")):
         self.__readableObject=readableObject
-        self.__filterExpression=filterExpression
+        self.__filterObject=filterObject
          
     # Python iterator osztály sajátosság. Kell és kész. :)       
     def __iter__(self):
         return self
     
     def next(self):
-        
         nextLine=self.__readableObject.readline()
-        while nextLine and not re.match(self.__filterExpression, nextLine):
+        while nextLine and not self.__filterObject.match(nextLine):
             nextLine=self.__readableObject.readline()
         if not nextLine:
             raise StopIteration
--- unittest/test_FilteredIterator.py
+++ unittest/test_FilteredIterator.py
@@ -7,26 +7,34 @@ Created on Jun 22, 2014
 
 import unittest
 
-class ListWithReadline(object):
-    def __init__(self, listobject):
-        self.__listobject=listobject
-        
+import fwloganalyzer.FilteredStream
+
+class ListWithReadline(list):
+
     def readline(self):
-        if len(self.__listobject)>0:
-            returnedObject=self.__listobject.pop(0)
-        else:
-            returnedObject=None
-        return returnedObject
+        if len(self)>0:
+            return self.pop(0)
+        return None
     
     
 class Test(unittest.TestCase):
 
     
-    def testLogIterator(self):
-        import fwloganalyzer.FilteredStream
+    def testLogIteratorWithAMockObject(self):
         testStrings=ListWithReadline([u"... kernel: DROP... - ezt nem szabad megtalálnia",'kernel: DROP ...', 'filler', 
                                       'text text text kernel: ACCEPT text text', 'DROP', 'ACCEPT',u"2014-05-12 14:33:55 localhost warning.kernel: DROP IN="])
         self.assertEqual(3, len([i for i in fwloganalyzer.FilteredStream(testStrings)]))
+    
+    def testLogIteratorWithAFile(self):
+        with open("kernel.log","r") as k:
+            self.assertEqual(521479, len([i for i in fwloganalyzer.FilteredStream(k)]))
+            
+    
+    def testLogIteratorWithNoMatchingRegex(self):
+        import re
+        with open("kernel.log","r") as k:
+            self.assertEqual(0, len([i for i in fwloganalyzer.FilteredStream(k, re.compile(r"^krixkrax-biztosan-nincs-talalat.*$"))]))
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testLogIterator']
